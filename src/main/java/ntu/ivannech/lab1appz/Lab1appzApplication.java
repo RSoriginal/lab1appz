db7dc13e6d80d0c1084bc2a1560c28e0e2200087
F1rstv5yer <f1rstv5yer@gmail.com>
2024-12-06 01:43:30 +0200

    empty project

---
+++ src/main/java/ntu/ivannech/lab1appz/Lab1appzApplication.java
@@ -0,0 +1,13 @@
+package ntu.ivannech.lab1appz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1appzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1appzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ntu/ivannech/lab1appz/Lab1appzApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.ivannech.lab1appz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1appzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
